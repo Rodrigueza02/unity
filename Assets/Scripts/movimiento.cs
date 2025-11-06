747bec71fb3d9241d37e62b4727d03e53d98c033
Rodrigueza02 <mariaj.rodrigueza@campusucc.edu.co>
2025-11-06 18:11:50 -0500

    scrp

---
+++ Assets/Scripts/movimiento.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MovimientoSimple : MonoBehaviour
+{
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * 5 * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * 5 * Time.deltaTime;
+        transform.Translate(x, 0, z);
+    }
+}
